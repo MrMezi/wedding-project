5277fb2fec1ebea171c8a4cf47f678b22438ee09
MrMezi <milosz_zal@outlook.com>
2023-07-02 16:54:53 +0100

    stlying and footer started

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <span>Made React.js by <Link to="https://github.com/MrMezi">@MrMezi</Link></span>
+      
+    </footer>
   )
 }
 
